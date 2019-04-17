0653045100df7ed0df9e125259ed92c6aa2ccb00
HossamOnsy <HossamEldeenOnsy@yahoo.com>
2019-04-17 15:03:57 +0200

    AlphaZolo Commit

--- js/animation.js
+++ js/animation.js
@@ -38,6 +38,9 @@ var aggronSprite = document.getElementById("aggron");
 var pokemonEgg = document.getElementById("pokemonEgg");
 var swoobatFireSprite = document.getElementById("swoobatFire");
 var skarmoryFireSprite = document.getElementById("skarmoryFire");
+var instinctImage=document.getElementById("instinct");
+var mysticImage=document.getElementById("mystic");
+var valorImage=document.getElementById("valor");
 
 
 var spaceKey = false;
@@ -45,6 +48,8 @@ var enterKey = false;
 var allShoot = [];
 var eggs = [];
 var enemies = [];
+var badgesArray=[];
+
 
 var backgroundScroll = new backgroundAnimate(0, 0, canvasW, canvasH);
 var backgroundScroll2 = new backgroundAnimate(canvasW, 0, canvasW, canvasH);
@@ -53,6 +58,9 @@ var playerCharacter = new character();
 
 var eggX = 50;
 var eggY = 30;
+
+var badgeX=canvasW-300;
+var badgeY=50;
 
 
 animation.prototype.startGame = function (character, level) {
@@ -140,7 +148,7 @@ function drawImage(key) {
   }
 
 
-  if (bonusCounter > bonusCondition) {
+  if (bonusCounter > bonusCondition) {    
     bonusCounter = 0;
     eggs.push(new lives(eggX, eggY))
     eggX += 50;
@@ -186,9 +194,13 @@ function drawImage(key) {
   
   setRandomEnemy();
   drawingEnemies();
+  badgesArray.forEach(function(badge)      ////////////////////////////////
+  {
+    badge.draw();
+  })
+
+  console.log(badgesArray);
   
-
-
 
 
   allShoot = allShoot.filter(function (fired) {
@@ -272,7 +284,7 @@ function gameOver() {
   context.font = "50pt Calibri";
   context.fillStyle = "white";
   context.fillText("Game Over", (canvasW / 2) - 140, canvasH / 2);
-  context.fillText("Press Enter to Try Again", (canvasW / 2) - 310, (canvasH / 2) + 60);
+  context.fillText("Press Enter to Try Again", (canvasW / 2) - 320, (canvasH / 2) + 60);
 
 }
 
@@ -282,25 +294,63 @@ function playerWin() {
   context.clearRect(0, 0, canvasW, canvasH);
   backgroundScroll.draw();
   backgroundScroll2.draw();
-
+  drawLargeBadges();
+  drawSmallBadges();
+  if(badgesArray.length>3){
+    drawCongrats();
+  }
+  else{
   context.font = "50pt Calibri";
   context.fillStyle = "white";
-  context.fillText("Winner", (canvasW / 2) - 80, canvasH / 2);
-  context.fillText("Press Enter to next level", (canvasW / 2) - 310, (canvasH / 2) + 60);
+  context.fillText("Winner", (canvasW / 2) - 80, 60);
+  context.fillText("Press Enter to next level", (canvasW / 2) - 310, (canvasH-90) );
   if (enterKey) {
     playAgain();
   }
 }
+}
+
+function drawCongrats(){
+  context.font = "50pt Calibri";
+  context.fillStyle = "white";
+  context.fillText("congratulation! you won the three badges", (canvasW / 2) - 575, 70);
+  context.fillText("Press Enter to play again", (canvasW / 2) - 325, (canvasH-90) );
+}
+
+function drawSmallBadges() {
+  console.log("dirx   = "+badgeX+" diry = "+badgeY);
+  
+  badgesArray.push(new badges(badgeX,badgeY,1,level))
+  badgeX+=50;
+  
+}
+
+function drawLargeBadges(){
+  newbadges=new badges(((canvasW/2)-180),((canvasH/2)-220),10,level);
+  newbadges.draw();
+}
 
 function playAgain() {
   if (enterKey == true && (playerCharacter.active == false) || playerCharacter.status) {
+    
+    
+    
+      char ++;
+      if(char==4)
+        char=1;
 
-    char = ((char+1)%3);
     level++;
+    if(level>3){
+      level=3;
+    }
     startGame(char, level);
   }
 }
 var mainInterval;
+
+function drawBadges(){
+
+}
 
 window.addEventListener("keypress", checkKey);
 
@@ -318,7 +368,7 @@ function checkKey(key) {
 
     enterKey = true;
 
-    if (!playerCharacter.active)
+    if (!playerCharacter.active||badgesArray.length>3)
       window.location.reload();
     else if (playerCharacter.status) {
       playAgain();
